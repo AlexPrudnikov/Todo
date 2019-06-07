fdead47aae9826e103d1e77955f13c3f5f720154
Shnur <Ra.Residentadvisor@gmail.com>
2019-06-08 00:33:26 +0300

    newProject

--- TodoCSharp/TodoDao/TodoDao.cs
+++ TodoCSharp/TodoDao/TodoDao.cs
@@ -52,7 +52,12 @@ namespace TodoCSharp.TodoDao
 
         public async Task RemoveAsync(Int32? id)
         {
-            
+            Todo todo = await context.Todos
+                .Where(t => t.TodoId == id)
+                .Include(x => x.Likes)
+                .FirstOrDefaultAsync();
+
+            context.Todos.Remove(todo);
 
             await context.SaveChangesAsync();
         }
